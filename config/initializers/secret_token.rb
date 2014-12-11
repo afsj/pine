89c811d75cf5ba144809cb421351ee139023874f
Juan Paulo Gutierrez <juanpaulo.gutierrez@gmail.com>
2014-12-11 23:19:37 +0900

    Fresh install of rails
    # rails new pine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pine::Application.config.secret_key_base = '00c546977c681c6049e8ab35197e6a6f3ecaa2d5bc24269589c1b83a8fc29ce35003bab0cb4b5528d2c438c68baa19975cbc0463380550a0a62ac088ba2256f2'
