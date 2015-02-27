2aa8f9d7ecdcf2a6289efba0aa8417ac3d1c1c31
Ron Bacalla <wronfreek@gmail.com>
2015-02-28 01:11:46 +0900

    display data in network table

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -2,5 +2,6 @@ class NetworkController < ApplicationController
   before_filter :authenticate_user!, :except => []  
   def index
     @countries = Country.all
+    @user = User.includes(:user_info)
   end
 end
