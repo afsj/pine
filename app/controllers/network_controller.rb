111a654a2fd01a6911f43d46fc445c00a30b67f3
Ron Bacalla <wronfreek@gmail.com>
2015-02-28 14:12:47 +0900

    display table alphabetically, blank entry last

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -2,7 +2,7 @@ class NetworkController < ApplicationController
   before_filter :authenticate_user!, :except => []  
   def index
     @countries = Country.all
-    @users = User.includes(:user_info)
+    @users = User.includes(:user_info).order("user_infos.first_name like '', user_infos.first_name")
     @major_fields = UserInfo.select(:major_field_name).distinct
     @minor_fields = UserInfo.select(:minor_field_name).distinct
   end
