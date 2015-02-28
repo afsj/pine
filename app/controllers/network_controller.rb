3e2a47ae69f85cf409bc8a1ab4f41bbcd2ef93ca
Ron Bacalla <wronfreek@gmail.com>
2015-02-28 16:26:32 +0900

    country filter

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -3,6 +3,7 @@ class NetworkController < ApplicationController
   def index
     @countries = Country.all
     @users = User.includes(:user_info).order("user_infos.first_name like '', user_infos.first_name").paginate(page: params[:page], per_page: 10)
+    @users = User.joins(:user_info).where("user_infos.residence_country like :country", {:country => params[:country]}).order("user_infos.first_name like '', user_infos.first_name").paginate(page: params[:page], per_page: 10)
     @major_fields = UserInfo.select(:major_field_name).distinct
     @minor_fields = UserInfo.select(:minor_field_name).distinct
   end
