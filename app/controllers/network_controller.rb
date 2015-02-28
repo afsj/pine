9e24eeb6de44093013a6eeffaca9d5d1824a8b12
Ron Bacalla <wronfreek@gmail.com>
2015-02-28 13:02:29 +0900

    added dropdown filters

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -2,6 +2,8 @@ class NetworkController < ApplicationController
   before_filter :authenticate_user!, :except => []  
   def index
     @countries = Country.all
-    @user = User.includes(:user_info)
+    @users = User.includes(:user_info)
+    @major_fields = UserInfo.select(:major_field_name).distinct
+    @minor_fields = UserInfo.select(:minor_field_name).distinct
   end
 end
